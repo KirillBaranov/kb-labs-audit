f8681930dba9b2d86af0bd83dbb096ff661de837
KirillBaranov <kirillBaranovJob@yandex.ru>
2025-10-31 15:47:38 +0300

    feat(audit): update audit-checks package
    Update index, types and build configuration

--- packages/audit-checks/src/index.ts
+++ packages/audit-checks/src/index.ts
@@ -1,9 +1,9 @@
-export * from './base.js';
-export { StyleCheck } from './style.js';
-export { TypesCheck } from './types.js';
-export { TestsCheck } from './tests.js';
-export { BuildCheck } from './build.js';
-export { DevLinkCheck } from './devlink.js';
-export { MindCheck } from './mind.js';
-export { SecurityCheck } from './security.js';
+export * from './base';
+export { StyleCheck } from './style';
+export { TypesCheck } from './types';
+export { TestsCheck } from './tests';
+export { BuildCheck } from './build';
+export { DevLinkCheck } from './devlink';
+export { MindCheck } from './mind';
+export { SecurityCheck } from './security';
 
